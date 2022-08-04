f852e1e30abf2392bb612b9137b74693495376c1
eduardKEKW <e.eduard13@yahoo.ro>
2022-08-04 17:59:00 +0300

    81513

--- public/index.php
+++ public/index.php
@@ -10,46 +10,46 @@
 |
 */
 
-$location = str_replace('\\', '/', getcwd());
-$currentLocation = explode("/", $location);
-$desiredLocation = implode("/", $currentLocation);
-$installFile = $desiredLocation . '/installer' . '/install.php';
+// $location = str_replace('\\', '/', getcwd());
+// $currentLocation = explode("/", $location);
+// $desiredLocation = implode("/", $currentLocation);
+// $installFile = $desiredLocation . '/installer' . '/install.php';
 
-if (file_exists($installFile)) {
-    $install = require __DIR__.'/installer/install.php';
-} else {
-    $install = null;
-}
+// if (file_exists($installFile)) {
+//     $install = require __DIR__.'/installer/install.php';
+// } else {
+//     $install = null;
+// }
 
-if (! is_null($install)) {
+// if (! is_null($install)) {
 
-    /*
-    |--------------------------------------------------------------------------
-    | Redirect To Installer Page
-    |--------------------------------------------------------------------------
-    |
-    | If somehow anything went wrong then this will redirect to the installer
-    | page.
-    |
-    */
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Redirect To Installer Page
+//     |--------------------------------------------------------------------------
+//     |
+//     | If somehow anything went wrong then this will redirect to the installer
+//     | page.
+//     |
+//     */
 
-    header("Location: $install");
+//     header("Location: $install");
 
-} else {
+// } else {
 
-    /*
-    |--------------------------------------------------------------------------
-    | Register The Auto Loader
-    |--------------------------------------------------------------------------
-    |
-    | Composer provides a convenient, automatically generated class loader for
-    | this application. We just need to utilize it! We'll simply require it
-    | into the script here so we don't need to manually load our classes.
-    |
-    */
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Register The Auto Loader
+//     |--------------------------------------------------------------------------
+//     |
+//     | Composer provides a convenient, automatically generated class loader for
+//     | this application. We just need to utilize it! We'll simply require it
+//     | into the script here so we don't need to manually load our classes.
+//     |
+//     */
 
     require __DIR__.'/../vendor/autoload.php';
-}
+// }
 
 define('LARAVEL_START', microtime(true));
 
